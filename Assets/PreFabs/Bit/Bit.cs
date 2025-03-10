9efa8605c78104de2897d992d32d771dfe613ddc
Jelle Sjollema <j.sjollema@ma-web.nl>
2025-03-10 18:02:39 +0100

    1.3.skill.3

--- Assets/PreFabs/Bit/Bit.cs
+++ Assets/PreFabs/Bit/Bit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bit : MonoBehaviour
 {
     SpriteRenderer spriteRender;
-    bool state = true;
+    public bool state = false;
 
     // Start is called before the first frame update
     void Start()
---
+++ Assets/PreFabs/Byte/Byte.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Byte : MonoBehaviour
+{
+    [SerializeField] Bit[] bits = new Bit[8];
+    [SerializeField] int value = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bin2dec();
+        
+    }
+
+    public void Bin2dec() 
+    {
+        this.value = 0;
+        if (bits[0].state) { value += 1; }
+        if (bits[1].state) { value += 2; }
+        if (bits[2].state) { value += 4; }
+        if (bits[3].state) { value += 8; }
+        if (bits[4].state) { value += 16;}
+        if (bits[5].state) { value += 32;}
+        if (bits[6].state) { value += 64; }
+        if (bits[7].state) { value += 128;}
+    }
+}
