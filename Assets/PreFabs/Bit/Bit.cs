ea15513df5f7a0b137f4574a302d8f5e8bc6e307
Jelle Sjollema <j.sjollema@ma-web.nl>
2025-03-10 11:48:53 +0100

    skill1.3.2

---
+++ Assets/PreFabs/Bit/Bit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bit : MonoBehaviour
+{
+    SpriteRenderer spriteRender;
+    bool state = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state)
+        {
+            spriteRender.color = Color.green;
+        } else
+        {
+            spriteRender.color = Color.red;
+        }
+        
+    }
+
+    private void OnMouseUp()
+    {
+        state = !state;
+    }
+}
